2e2cd8a326d00334ab1bf032035442255494128b
Nazmul Hasan <nazmulimm@gmail.com>
2020-09-29 19:10:28 +0600

    Final Update

---
+++ VirtualButton/Assets/Scripts/CaptureImage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureImage : MonoBehaviour
+{
+    public Camera snapCam;
+    int resWidth , resHeight;
+    public GameObject fleshImage;
+    private void Start()
+    {
+        snapCam = GetComponent<Camera>();
+    }
+    public void TakePicture()
+    {
+        resWidth = Screen.width;
+        resHeight = Screen.height;
+        Debug.Log("Width: " + resWidth + " Height: " + resHeight);
+        snapCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        StartCoroutine(TakeSnapShot());
+    }
+
+    private IEnumerator TakeSnapShot()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D snapShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        snapCam.Render();
+        RenderTexture.active = snapCam.targetTexture;
+        snapShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        byte[] bytes = snapShot.EncodeToPNG();
+        string fileName = string.Format("{0}/SnapShots/{1}_{2}.png",Application.dataPath, Application.productName, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+        System.IO.File.WriteAllBytes(fileName, bytes);
+        Debug.Log("Snapshot Taken");
+        snapCam.targetTexture = null;
+        fleshImage.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(.2f);
+        fleshImage.SetActive(false);
+    }
+
+  
+}
---
+++ VirtualButton/Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    public Transform cloudRotate;
+    public GameObject AugmentedOBJ;
+    private bool enaBTN = false;
+    private bool aniBTN = false;
+    void Start()
+    {
+        VirtualButtonBehaviour[] virtualBTN = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for(int i = 0; i< virtualBTN.Length; i++)
+        {
+            virtualBTN[i].RegisterOnButtonPressed(onPressed);
+            virtualBTN[i].RegisterOnButtonReleased(onReleased);
+        }
+    }
+
+    private void Update()
+    {
+        if (aniBTN)
+        {
+            cloudRotate.Rotate(0, 1, 0);
+        }
+    }
+
+    private void onPressed(VirtualButtonBehaviour vb)
+    {
+        string name = vb.name;
+        if(name == "Ani-BTN")
+        {
+            aniBTN = true;
+        }
+        else if(name == "Enable-BTN"){
+            enaBTN = !enaBTN;
+            AugmentedOBJ.SetActive(!enaBTN);
+        }
+    }
+
+    private void onReleased(VirtualButtonBehaviour vb)
+    {
+        string name = vb.name;
+        if (name == "Ani-BTN")
+        {
+            aniBTN = false;
+        }
+    }
+}
